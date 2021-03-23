183b37e5791659ae33de9478a18a43e2ce45006f
Carter Woneymaker <carter.woneymaker@outlook.com>
2021-03-23 12:28:52 -0400

    Sprint 1

---
+++ AccountManager.js
@@ -0,0 +1,17 @@
+class AccountManager{
+    constructor(){
+        //this is like a Python {} dictionary
+        //or like a C++ std::map<>
+        //key = email address
+        //value = password
+        this.accounts = new Map();
+    }
+    addAccount(email,password){
+        if(this.accounts.has(email))
+            return false;
+        this.accounts.set(email,password);
+        return true;
+    }
+}
+
+exports.AccountManager = AccountManager;
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ETEC-2104-ScrumBoard
 1/19
+
+i know this is all stuff that was done in class but it covers all my contitions of satisfactions except the fromt end button that requires frmt end stuff to be done first
