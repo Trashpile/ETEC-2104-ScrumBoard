2d1ac3553a005b473c59bfb93c6016773027afb9
Trashpile <vuuluxius@gmail.com>
2021-03-25 17:40:52 -0400

    Stage1 - it doesn't blow up!

--- memeManager.js
+++ memeManager.js
@@ -1,37 +1,72 @@
-class Meme
-{
-    constructor(likes, name)
-    {
-        this.likes = likes;
+exports.MemeManager = MemeManager;
+
+
+
+class Meme{
+    constructor(/*possible list of tags?*/memeImage, isFavorite, creatorID, name){
+        this.image = memeImage;
+        this.favorite = isFavorite;
+        this.creator = creatorID;
+        this.likes = 0;
         this.name = name;
     }
 }
-class MemeManager
-{
-    constructor()
-    {
-        this.mData=[];
+class MemeManager{
+    constructor(){
+        this.memeData = [];
     }
-///Let the most liked meme rise to the top via Bubble sort.  Will refactor to sort by other means with merge sort.
+    addMeme(memeImageTemp,creatorID)
+    {
+        this.memeImage = memeImageTemp;
+        this.favorite = false;
+        if(creatorID)
+            this.creator = creatorID;
+    }
+    addFavorite(memeImageTemp){
+        if(!(this.memeData.has(memeImageTemp)))
+            this.addMeme(memeImageTemp);
+        this.memeData.get(memeImageTemp).isFavorite = true;
+    }
+    getMeme(index){
+        const memeArray = Array.from(this.memeData);
+        if(!(memeArray[index])){
+            return null;
+        }
+        return memeArray[index].memeImage;
+    }
+    getFavorite(index){
+        const memeArray = Array.from(this.memeData);
+        if(!(memeArray[index])){
+            return null;
+        }
+        else if(memeArray[index].isFavorite == false)
+        {
+            return 0;
+        }
+        else
+        {
+            return memeArray[index].memeImage;
+        }
+    }
+    //Sort the meme map by the likes
     sortByLike()
     {
-        
-        let mData = this.mData;//So we don't have to reference this. each call
-        for(let i = 0; i< mData.length - 1; i++)
+        //Convert into list to sort.
+        let memeData = [];//So we don't have to reference this. each call
+        for(let i = 0; i< memeData.length - 1; i++)
         {
             
-            for(let j = 0; j < mData.length - 1; j++)
+            for(let j = 0; j < memeData.length - 1; j++)
             {
-                if(mData[j].likes < mData[j+1].likes)
+                if(memeData[j].likes < memeData[j+1].likes)
                 {
-                    let temp = mData[j];
-                    mData[j] = mData[j+1];
-                    mData[j+1] = temp;
+                    let temp = memeData[j];
+                    memeData[j] = memeData[j+1];
+                    memeData[j+1] = temp;
                 }
             }
         }
     }
-    
 }
-exports.Meme = Meme;
 exports.MemeManager = MemeManager;
+exports.Meme = Meme;
