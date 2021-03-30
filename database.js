1f53b0a8029188c86e54aeda76e657e454f345f2
EthanEdelen <graetu2000@gmail.com>
2021-03-30 17:08:12 -0400

    -Changed the tag table and added a tagAlias table, also some enums for later.

--- database.js
+++ database.js
@@ -3,6 +3,22 @@ sqlite3 = require("sqlite3").verbose();
 let conn = new sqlite3.Database("./priv/memedepository.sql");
 let name = "Bob";
 let password = "s3cr3t";
+
+const tagType = {
+    Official: 1,
+    Unofficial: 2,
+    Internal: 3,
+}
+
+const language = {
+    English: 1,
+    Spanish: 2,
+    French: 3,
+    German: 4,
+    Japanese: 5,
+    Chinese: 6, 
+}
+
 function main()
 {
     conn.serialize(); //Says everytime you submit something to database, it'll force it to run sequentially.
@@ -20,7 +36,7 @@ function main()
                 name varchar(32),
                 password varchar(32),
                 avatar blob)`,
-            {}, //Parameters
+            {}, //Initial parameters. Empty for now.
             (e) => {
                 console.log("error is:",e) //Runs if err, null if not
             }
@@ -32,20 +48,34 @@ function main()
             uid integer foreign key,
             tid integer foreign key
             avatar blob)`,
-        {}, //Parameters
+        {}, //Initial parameters. Empty for now.
         (e) => {
             console.log("error is:",e) //Runs if err, null if not
         });
-        //Insert enum for tag type.
+        //In the js, the tagType will be an enum. In the database, it will be an integer.
         conn.run( `create table tags(
-            tid integer primary key ,
-            name varchar(32),
+            tID integer primary key ,
+            tagContent varchar(32),
+            tagType integer
             )`,
-        {}, //Parameters
+        {}, //Initial parameters. Empty for now.
         (e) => {
             console.log("error is:",e) //Runs if err, null if not
         }
-    );
+        );
+        conn.run( `create table tagAlias(
+            t1ID integer foreign key,
+            t2ID integer foreign key,
+            t1Lang integer,
+            t2Lang integer,
+            primeAlias integer foreign key
+            )`,
+        {}, //Initial parameters. Empty for now.
+        (e) => {
+            console.log("error is:",e) //Runs if err, null if not
+        }
+        );
+        //This should probably be later removed; Database manipulation should happen elsewhere. This file just creates it.
         conn.run( "insert into users (name, password) values ($name, $passwd)",
             { $name: name, $passwd: password }, //Parameters - use the $ sign in .run()
             (e) => {  
