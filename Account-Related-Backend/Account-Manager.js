601850dc74531af338d9bc847c480490d8a8e919
LewisWilliam2001 <WilliamLewis2001@outlook.com>
2021-04-11 21:48:57 -0400

    Full Site high Organisation

---
+++ Account-Related-Backend/Account-Manager.js
@@ -0,0 +1,42 @@
+class AccountManager
+{
+    constructor()
+    {
+        //this is like a Python {} dictionary
+        //or like a C++ std::map<>
+        //key = email address
+        //value = password
+        this.accounts = new Map();
+    }
+    addAccount(email,password)
+    {
+        if(this.accounts.has(email))
+            return false;
+        let u = new User(email,password);
+        this.accounts.set(email,u);
+        return true;
+        //returns true if made, false if already exits.
+    }
+    removeAccount(email,password)
+    {
+        //To Be done
+    }
+    changeAccountPassword(email,password)
+    {
+        //To Be done
+    }
+    changeAccountEmail(email,password)
+    {
+        //To Be done
+    }
+    getAvatar(email)
+    {
+        return this.accounts.get(email).avatar;//returns Avatar of email
+    }
+    setAvatar(email, newavatar)
+    {
+        return this.accounts.get(email).avatar = newavatar;//sets avatar of given email
+    }
+}
+
+exports.AccountManager = AccountManager;
---
+++ Account-Related-Backend/User-Var3.js
@@ -0,0 +1,9 @@
+class UserVar3{
+    constructor(uname,passwd){
+        this.username = uname;
+        this.password = passwd;
+        this.avatar = null;
+        //Add code to store userID here?
+    }
+}
+exports.UserVar3 = UserVar3;
