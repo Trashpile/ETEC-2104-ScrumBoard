df920d6374906e455a5b83eaa6f618119f67bb07
WhiteChicken7 <55899554+WhiteChicken7@users.noreply.github.com>
2021-04-12 20:32:01 -0400

    revamped friend system and added more functionality

--- Account.js
+++ Account.js
@@ -6,95 +6,186 @@ class Account
     {
         this.id = uID;
         this.username = uname;
-        this.follow = new Map();
-        this.followers = new Map();
-        this.followerRequests = new Map();
-        this.private = isPrivate;
+        //this.follow = new Map();
+        //this.followers = new Map();
+        //this.followerRequests = new Map();
+        //this.private = isPrivate;
+        this.friends = new Map();
+        this.requestsReceived = new Map();
+        this.requestsSent = new Map();
     }
 
-    followNewUser( uID, other )
+    sendFriendRequest(uID, other)
     {
-        // adds this user to other user's follower list
-        if (uID === this.id)
+        if (uID == this.id || this.friends.has(uID))
         {
-            return false;
-        }
-        else if (this.follow.has(uID))
-        {
-            return false;
-        }
-        else if ( other.private )
-        {
-            // send friend request
-            other.followerRequests.set( this.id, this);
             return false;
         }
         else
         {
-            this.follow.set(uID, other);
-            other.newFollower(this.id, this);
+            this.requestsSent.set(uID, other);
+            other.requestsReceived.set(this.id, this);
+            return true;
+
+        }
+    }
+
+    acceptFriendRequest(uID, other)
+    {
+        if (uID == this.id)
+        {
+            return false;
+        }
+        else if (this.requestsReceived.has(uID))
+        {
+            this.friends.set(uID, other);
+            other.friends.set(this.id, this);
+            this.requestsReceived.delete(uID)
+            other.requestsSent.delete(this.id);
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    isFriend(uID, other)
+    {
+        return this.friends.has(uID);
+    }
+
+    hasFriendRequest(uID, other)
+    {
+        return this.requestsReceived.has(uID);
+    }
+
+    removeFriend(uID, other)
+    {
+        if (uID == this.id || this.friends.has(uID) == false)
+        {
+            return false;
+        }
+        else if (this.friends.has(uID))
+        {
+            this.friends.delete(uID);
+            other.friends.delete(this.id);
             return true;
         }
-
     }
 
-    newFollower( uID, other)
+    deleteSentRequest(uID, other)
     {
-        // add other user to this user's follower list
-        if (uID === this.id)
+        if (uID == this.id || this.requestsSent.has(uID) == false)
         {
             return false;
         }
-        else if (this.followers.has(uID))
+        else if (this.requestsSent.has(uID))
         {
-            return false;
-        }
-        else
-        {
-            this.followers.set(uID, other);
+            this.requestsSent.delete(uID);
+            other.requestsReceived.delete(this.id);
             return true;
         }
     }
 
-    isFollower( uID, other )
+    ignoreReceivedRequest(uID, other)
     {
-        // checks if other is a follower of this user
-        if (this.followers.has(uID))
+        if (uID == this.id || this.requestsReceived.has(uID) == false)
         {
+            return false;
+        }
+        else if (this.requestsReceived.has(uID))
+        {
+            this.requestsReceived.delete(uID);
+            other.requestsSent.delete(this.id);
             return true;
         }
-        else
-        {
-            return false;
-        }
     }
 
-    isPrivate()
-    {
-        // checks if this user is private
-        return this.private;
-    }
+    // followNewUser( uID, other )
+    // {
+    //     // adds this user to other user's follower list
+    //     if (uID === this.id)
+    //     {
+    //         return false;
+    //     }
+    //     else if (this.follow.has(uID))
+    //     {
+    //         return false;
+    //     }
+    //     else if ( other.private )
+    //     {
+    //         // send friend request
+    //         other.followerRequests.set( this.id, this);
+    //         return false;
+    //     }
+    //     else
+    //     {
+    //         this.follow.set(uID, other);
+    //         other.newFollower(this.id, this);
+    //         return true;
+    //     }
 
-    setPrivacy( isPrivate )
-    {
-        // private is a bool
-        // true makes account private
-        // false makes account public
-        // sets a user to private or public
-        this.private = isPrivate;
-        if (isPrivate === true)
-        {
-            return this.private;
-        }
-        else if (isPrivate === false)
-        {
-            return !this.private;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    // }
+
+    // newFollower( uID, other)
+    // {
+    //     // add other user to this user's follower list
+    //     if (uID === this.id)
+    //     {
+    //         return false;
+    //     }
+    //     else if (this.followers.has(uID))
+    //     {
+    //         return false;
+    //     }
+    //     else
+    //     {
+    //         this.followers.set(uID, other);
+    //         return true;
+    //     }
+    // }
+
+    // isFollower( uID, other )
+    // {
+    //     // checks if other is a follower of this user
+    //     if (this.followers.has(uID))
+    //     {
+    //         return true;
+    //     }
+    //     else
+    //     {
+    //         return false;
+    //     }
+    // }
+
+    // isPrivate()
+    // {
+    //     // checks if this user is private
+    //     return this.private;
+    // }
+
+    // setPrivacy( isPrivate )
+    // {
+    //     // private is a bool
+    //     // true makes account private
+    //     // false makes account public
+    //     // sets a user to private or public
+    //     this.private = isPrivate;
+    //     if (isPrivate === true)
+    //     {
+    //         return this.private;
+    //     }
+    //     else if (isPrivate === false)
+    //     {
+    //         return !this.private;
+    //     }
+    //     else
+    //     {
+    //         return false;
+    //     }
+    // }
 }
 
 exports.Account = Account;
--- test/followtests.js
+++ test/followtests.js
@@ -13,62 +13,248 @@ describe("Account Tests", () => {
         S = new Account("TheShadowMan", 1001, false);
     });
 
-    describe("followNewUser", () => {
-        it("should return true if user successfully follows other user", () => {
-            assert.strictEqual( W.followNewUser(S.id, S), true);
+    describe("sendFriendRequest", () => {
+        it("should return true if user sends friend request to other user", () => {
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
         });
     });
 
-    describe("followNewUser", () => {
-        it("should return false if user tries to follow their self", () => {
-            assert.strictEqual( W.followNewUser(W.id, W), false);
+    describe("sendFriendRequest", () => {
+        it("should return false if user tries to send friend request to itself", () => {
+            assert.strictEqual( W.sendFriendRequest(W.id, W), false);
         });
     });
 
-    describe("followNewUser", () => {
-        it("should return false if user tries to follow someone they are already following", () => {
-            assert.strictEqual( W.followNewUser(S.id, S), true);
-            assert.strictEqual( W.followNewUser(S.id, S), false);
+    describe("sendFriendRequest", () => {
+        it("should return false if user tries to send friend request to an existing friend", () => {
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
+            assert.strictEqual( S.acceptFriendRequest(W.id, W), true);
+            assert.strictEqual( W.isFriend(S.id, S), true);
+            assert.strictEqual( W.sendFriendRequest(S.id, S), false);
         });
     });
 
-    describe("isFollower", () => {
-        it("should return true if user is in other user's follower list", () => {
-            assert.strictEqual( S.isFollower(W.id, W), false);
-            assert.strictEqual( W.followNewUser(S.id, S), true);
-            assert.strictEqual( S.isFollower(W.id, W), true);
+    describe("acceptFriendRequest", () => {
+        it("should return true if user accepts a friend request", () => {
+            assert.strictEqual( S.sendFriendRequest(W.id, W), true);
+            assert.strictEqual( W.acceptFriendRequest(S.id, S), true);
         });
     });
 
-    describe("isPrivate", () => {
-        it("should return true if user is set to private", () => {
-            assert.strictEqual( S.isPrivate(), false);
-            assert.strictEqual( S.setPrivacy(true), true);
-            assert.strictEqual( S.isPrivate(), true);
+    describe("acceptFriendRequest", () => {
+        it("should return false if user tries to accepts a friend request from itself", () => {
+            assert.strictEqual( W.acceptFriendRequest(W.id, W), false);
         });
     });
 
-    describe("isPrivate", () => {
-        it("should return true if user is set to public", () => {
-            assert.strictEqual( S.isPrivate(), false);
-            assert.strictEqual( S.setPrivacy(true), true);
-            assert.strictEqual( S.isPrivate(), true);
-            assert.strictEqual( S.setPrivacy(false), true);
-            assert.strictEqual( S.isPrivate(), false);
+    describe("acceptFriendRequest", () => {
+        it("should return false if user tries to accepts a friend request from a user who did not send one", () => {
+            assert.strictEqual( W.hasFriendRequest(S.id, S), false);
+            assert.strictEqual( W.acceptFriendRequest(S.id, S), false);
         });
     });
 
-    describe("setPrivacy", () => {
-        it("should return false if setPrivacy is not given a bool", () => {
-            assert.strictEqual( S.setPrivacy("true"), false);
+    describe("sendFriendRequest", () => {
+        it("should return false if user tries to accept a friend request from an existing friend", () => {
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
+            assert.strictEqual( S.acceptFriendRequest(W.id, W), true);
+            assert.strictEqual( W.isFriend(S.id, S), true);
+            assert.strictEqual( W.acceptFriendRequest(S.id, S), false);
         });
     });
 
-    describe("followNewUser", () => {
-        it("should return false if other user is private", () => {
-            assert.strictEqual( S.setPrivacy(true), true);
-            assert.strictEqual( W.followNewUser(S.id, S), false);
+    describe("isFriend", () => {
+        it("should return true if user is friends with other user", () => {
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
+            assert.strictEqual( S.acceptFriendRequest(W.id, W), true);
+            assert.strictEqual( W.friends.has(S.id), true);
+            assert.strictEqual( W.isFriend(S.id, S), true);
         });
     });
+
+    describe("isFriend", () => {
+        it("should return false if other has not accepted friend request", () => {
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
+            assert.strictEqual( W.friends.has(S.id), false);
+            assert.strictEqual( W.isFriend(S.id, S), false);
+        });
+    });
+
+    describe("isFriend", () => {
+        it("should return false if user is not friends with other user", () => {
+            assert.strictEqual( W.friends.has(S.id), false);
+            assert.strictEqual( W.isFriend(S.id, S), false);
+        });
+    });
+
+    describe("hasFriendRequest", () => {
+        it("should return true if user has friend request from other user", () => {
+            assert.strictEqual( S.hasFriendRequest(W.id, W), false);
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
+            assert.strictEqual( S.hasFriendRequest(W.id, W), true);
+        });
+    });
+
+    describe("hasFriendRequest", () => {
+        it("should return false if user does not have friend request from other user", () => {
+            assert.strictEqual( S.requestsReceived.has(W.id), false);
+            assert.strictEqual( S.hasFriendRequest(W.id, W), false);
+        });
+    });
+
+    describe("hasFriendRequest", () => {
+        it("should return false if user accepted friend request from other user", () => {
+            assert.strictEqual( S.requestsReceived.has(W.id), false);
+            assert.strictEqual( S.hasFriendRequest(W.id, W), false);
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
+            assert.strictEqual( S.hasFriendRequest(W.id, W), true);
+            assert.strictEqual( S.acceptFriendRequest(W.id, W), true);
+            assert.strictEqual( S.hasFriendRequest(W.id, W), false);
+
+        });
+    });
+
+    describe("removeFriend", () => {
+        it("should return true if user removes other user as a friend", () => {
+            assert.strictEqual( W.isFriend(S.id, S), false);
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
+            assert.strictEqual( S.acceptFriendRequest(W.id, W), true);
+            assert.strictEqual( W.isFriend(S.id, S), true);
+            assert.strictEqual( S.isFriend(W.id, W), true);
+            assert.strictEqual( W.removeFriend(S.id, S), true);
+            assert.strictEqual( W.isFriend(S.id, S), false);
+            assert.strictEqual( S.isFriend(W.id, W), false);
+        });
+    });
+
+    describe("removeFriend", () => {
+        it("should return false if other user is not a friend", () => {
+            assert.strictEqual( W.removeFriend(S.id, S), false);
+        });
+    });
+
+    describe("removeFriend", () => {
+        it("should return false user tries to unfriend itself", () => {
+            assert.strictEqual( W.removeFriend(W.id, W), false);
+        });
+    });
+
+    describe("deleteSentRequest", () => {
+        it("should return true if sent friend request is deleted", () => {
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
+            assert.strictEqual( S.hasFriendRequest(W.id, W), true);
+            assert.strictEqual( W.deleteSentRequest(S.id, S), true);
+            assert.strictEqual( S.hasFriendRequest(W.id, W), false);
+        });
+    });
+
+    describe("deleteSentRequest", () => {
+        it("should return false if user tries to delete a request to itself", () => {
+            assert.strictEqual( W.deleteSentRequest(W.id, W), false);
+        });
+    });
+
+    describe("deleteSentRequest", () => {
+        it("should return false if user tries to delete a request that does not exist", () => {
+            assert.strictEqual( W.deleteSentRequest(S.id, S), false);
+        });
+    });
+
+    describe("deleteSentRequest", () => {
+        it("should return false if user tries to delete a request that was accepted", () => {
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
+            assert.strictEqual( S.acceptFriendRequest(W.id, W), true);
+            assert.strictEqual( W.deleteSentRequest(S.id, S), false);
+        });
+    });
+
+    // break
+
+    describe("ignoreReceivedRequest", () => {
+        it("should return true if received friend request is ignored", () => {
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
+            assert.strictEqual( S.hasFriendRequest(W.id, W), true);
+            assert.strictEqual( S.ignoreReceivedRequest(W.id, W), true);
+            assert.strictEqual( S.hasFriendRequest(W.id, W), false);
+        });
+    });
+
+    describe("ignoreReceivedRequest", () => {
+        it("should return false if user tries to delete a request from itself", () => {
+            assert.strictEqual( W.ignoreReceivedRequest(W.id, W), false);
+        });
+    });
+
+    describe("ignoreReceivedRequest", () => {
+        it("should return false if user tries to ignore a request that does not exist", () => {
+            assert.strictEqual( W.ignoreReceivedRequest(S.id, S), false);
+        });
+    });
+
+    describe("ignoreReceivedRequest", () => {
+        it("should return false if user tries to ignore a request that was accepted", () => {
+            assert.strictEqual( W.sendFriendRequest(S.id, S), true);
+            assert.strictEqual( S.acceptFriendRequest(W.id, W), true);
+            assert.strictEqual( S.ignoreReceivedRequest(W.id, W), false);
+        });
+    });
+
+    // describe("followNewUser", () => {
+    //     it("should return true if user successfully follows other user", () => {
+    //         assert.strictEqual( W.followNewUser(S.id, S), true);
+    //     });
+    // });
+
+    // describe("followNewUser", () => {
+    //     it("should return false if user tries to follow their self", () => {
+    //         assert.strictEqual( W.followNewUser(W.id, W), false);
+    //     });
+    // });
+
+    // describe("followNewUser", () => {
+    //     it("should return false if user tries to follow someone they are already following", () => {
+    //         assert.strictEqual( W.followNewUser(S.id, S), true);
+    //         assert.strictEqual( W.followNewUser(S.id, S), false);
+    //     });
+    // });
+
+    // describe("isFollower", () => {
+    //     it("should return true if user is in other user's follower list", () => {
+    //         assert.strictEqual( S.isFollower(W.id, W), false);
+    //         assert.strictEqual( W.followNewUser(S.id, S), true);
+    //         assert.strictEqual( S.isFollower(W.id, W), true);
+    //     });
+    // });
+
+    // describe("isPrivate", () => {
+    //     it("should return true if user is set to private", () => {
+    //         assert.strictEqual( S.isPrivate(), false);
+    //         assert.strictEqual( S.setPrivacy(true), true);
+    //         assert.strictEqual( S.isPrivate(), true);
+    //     });
+    // });
+
+    // describe("isPrivate", () => {
+    //     it("should return true if user is set to public", () => {
+    //         assert.strictEqual( S.isPrivate(), false);
+    //         assert.strictEqual( S.setPrivacy(true), true);
+    //         assert.strictEqual( S.isPrivate(), true);
+    //         assert.strictEqual( S.setPrivacy(false), true);
+    //         assert.strictEqual( S.isPrivate(), false);
+    //     });
+    // });
+
+    // describe("setPrivacy", () => {
+    //     it("should return false if setPrivacy is not given a bool", () => {
+    //         assert.strictEqual( S.setPrivacy("true"), false);
+    //     });
+    // });
+
+    // describe("followNewUser", () => {
+    //     it("should return false if other user is private", () => {
+    //         assert.strictEqual( S.setPrivacy(true), true);
+    //         assert.strictEqual( W.followNewUser(S.id, S), false);
+    //     });
+    // });
 
 });
