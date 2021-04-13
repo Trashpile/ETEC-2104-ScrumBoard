31e5ff01c3dbc0b3da7433d03ca2463d0c0b73f1
CattaLovea <CattaLovea@users.noreply.github.com>
2021-04-12 22:28:28 -0400

    This is my commit for sprint2

--- app.js
+++ app.js
@@ -15,7 +15,20 @@ function startServer(){
         saveUninitialized: false,
         secret: "fojogjhawogfjuaewouwo"
     }));
+function check(req){
+    if(req!=undefined)
+    {
+        if(req.session.theme==undefined)
+            req.session.theme = "light";
 
+        if(req.session.privacy==undefined)
+            req.session.privacy = false;
+
+        if(req.session.bio==undefined)
+            req.session.bio = "This user has no bio";
+    }
+      
+}
     let accountManager = new AccountManager.AccountManager();
 
         app.set("view engine", "ejs");
@@ -25,53 +38,55 @@ function startServer(){
     });
 
     app.get("/home", (req,res) => {
-        res.render( "templates/home.ejs" );
+        if(req!= undefined)
+        {
+            check(req);
+            res.render( "templates/home.ejs",
+            {theme: req.session.theme});
+        }
+        else
+        {
+            res.render( "templates/home.ejs",
+            {theme: "light"});
+        }
+       
     });
 
     app.get("/profile", (req,res) => {
         let name = req.query["name"];
-        let currentUser = req.session.username;
-        let bio = req.session.bio;
-        //bio = "THIS IS MY BIO!";
-        //req.session.privacy = true;
-        let privacy = req.session.privacy;
-        res.render( "templates/profile.ejs",
-        {username: name, currentUser: currentUser, privacy: privacy, bio: req.session.bio});       
+        if(req!=undefined)
+        {
+            let currentUser = req.session.username;
+            check(req)
+            res.render( "templates/profile.ejs",
+            {username: name, currentUser: currentUser, privacy: req.session.privacy, bio: req.session.bio, theme: req.session.theme});       
+        }
+        else{
+            res.render( "templates/profile.ejs",
+            {username: name, currentUser: "Guest", privacy: false, bio: "This user does not have a bio", theme: "light"});       
+        }
     });
 
     app.get("/settings", (req,res) => {
-        let currentUser = req.session.username;
-        req.session.privacy = false;
-        let privacy = req.session.privacy;
-        let changeSettings = true;
-        req.session.bio = "bio";
-        let bio = req.session.bio;
-        req.session.regenerate( () => {
-            if(changeSettings)
-            {
-                req.session.username = req.query["currentUser"];
-                req.session.privacy = req.query["privacy"];
-                req.session.bio = req.query["bio"];
+        check(req);
+        req.session.username = req.query["currentUser"];
+        req.session.privacy = req.query["privacy"];
+        req.session.bio = req.query["bio"];
+        req.session.theme = req.query["theme"];
 
-                currentUser = req.session.username;
-                privacy = req.session.privacy;
-                changeSettings = false;
-                bio = req.session.bio;
-                res.render( "templates/settings.ejs",
-                {currentUser: currentUser, privacy: privacy, edit:changeSettings, bio: bio});
-                return;
-            }
-            
-        })
-        
+        let currentUser = req.session.username;
+        let privacy = req.session.privacy;
+        let bio = req.session.bio;
+        let theme = req.session.theme;
+        res.render( "templates/settings.ejs",
+        {currentUser: currentUser, privacy: privacy, bio: bio, theme:theme});
     });
 
     app.get("/register", (req,res)=>{
         let uname = req.query["username"];
         let passwd = req.query["password"];
-        
         if( uname === undefined && passwd === undefined ){
-            res.render( "templates/register.ejs" );
+            res.render( "templates/register.ejs", {theme:req.session.theme} );
             return;
         }
         
@@ -83,13 +98,14 @@ function startServer(){
         if( accountManager.addAccount(uname,passwd)){
             req.session.regenerate( () => {
                 req.session.username = uname;
-                //res.send("Account registered! You may now go to your profile by clicking on your avatar in the top right corner of the screen.");
                 req.session.privacy = false;
                 let privacy = req.session.privacy;
-                req.session.bio = "This user does not have a bio yet!"
-                let bio = req.session.bio
+                req.session.bio = "This user does not have a bio yet!";
+                let bio = req.session.bio;
+                req.session.theme = "light";
+                let theme = req.session.theme;
                 res.render( "templates/profile.ejs",
-                {username: uname, currentUser: uname, privacy: privacy, bio: bio});
+                {username: uname, currentUser: uname, privacy: privacy, bio: bio, theme:theme});
                 return;
 
             });
@@ -98,7 +114,14 @@ function startServer(){
 
 
     });
-
+    app.get("/who", (req,res) => {
+        if(req.session && req.session.username){
+            let currentUser = req.session.username;
+            res.send(currentUser);
+        } else {
+            res.send("");
+        }
+    });
    
     let srv = app.listen(2021);
     return srv;
